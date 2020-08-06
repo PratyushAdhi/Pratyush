e358b6ea90adb380aca4c18780238f4f4ce34d39
Pratyush <pratyushadhikary1152@gmail.com>
2020-08-06 11:52:09 +0530

    database

--- Pratyush/settings.py
+++ Pratyush/settings.py
@@ -173,8 +173,4 @@ STATICFILES_DIRS = (
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
+DATABASES = {'default': dj_database_url.config()}
